2f08e97a02935dd82a9dfe6d345fb968d056a90e
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-02-13 15:41:04 -0300

    feat: faz a conecao com o banco de dados

---
+++ src/main/java/br/com/centroweg/Main.java
@@ -0,0 +1,18 @@
+package br.com.centroweg;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    private static String URL = "jdbc:mysql://localhost:3306/fiscalflow?useSSL=false&serverTimezone=UTC";
+    private static String USER = "root";
+    private static String PASSWORD = "mysqlPW";
+    public static void main(String[] args) {
+
+
+    }
+}
