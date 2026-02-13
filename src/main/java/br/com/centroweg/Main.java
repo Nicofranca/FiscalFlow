3a078a49cbd759a9719b0ba6f02c5cd1e1d25b1b
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-13 15:38:38 -0300

    feat: inicia arquitetura do sistema

--- src/main/java/br/com/centroweg/Main.java
+++ src/main/java/br/com/centroweg/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package br.com.centroweg;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
