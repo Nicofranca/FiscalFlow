65e2fcef4598ee612b17afd2ea7fedbffa913a0a
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-13 21:08:23 -0300

    feat: cria Conexao

--- src/main/java/br/com/centroweg/infra/database/Conexao.java
+++ src/main/java/br/com/centroweg/infra/database/Conexao.java
@@ -1,5 +1,16 @@
 package br.com.centroweg.infra.database;
 
-public class Conexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class Conexao {
+    private static String URL = "jdbc:mysql://localhost:3306/fiscalflow?useSSL=false&serverTimezone=UTC";
+    private static String USER = "root";
+    private static String PASSWORD = "mysqlPW";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
 }
