865006fa9485d55763a083c4adb4edec3dd5f6f1
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-27 14:30:11 -0300

    feat: estrutura Spring Boot usando SOLID e DDD

---
+++ FiscalFlowSpring/src/main/java/com/weg/FiscalFlowSpring/FiscalFlowSpringApplication.java
@@ -0,0 +1,13 @@
+package com.weg.FiscalFlowSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalFlowSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalFlowSpringApplication.class, args);
+	}
+
+}
---
+++ FiscalFlowSpring/src/test/java/com/weg/FiscalFlowSpring/FiscalFlowSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.FiscalFlowSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalFlowSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
